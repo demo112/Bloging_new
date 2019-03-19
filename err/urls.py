d4ac8b8324774aa33cc17f30071b845dfa093fa1
cooper <huafengdongji@hotmail.com>
2019-03-19 21:36:42 +0800

    创建应用： 文章 报错

---
+++ err/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.conf.urls import url, include
+from . import views
+app_name = 'err'
+
+urlpatterns = [
+    path('', include('views.back_index', name='back_index')),
+]
