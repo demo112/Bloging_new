4129140a581b3c0ea253ea2512b47e1cf0431a61
cooper <huafengdongji@hotmail.com>
2019-03-22 07:40:03 +0800

    设置为开发环境

--- Bloging/settings.py
+++ Bloging/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(eo&-5*)p-n4s0p77v&g6#&(^eie=4lo7quqxde4l7ckce4c1m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
